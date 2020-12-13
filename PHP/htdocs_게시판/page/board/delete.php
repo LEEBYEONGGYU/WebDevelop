d7f990590a6fd706553f4cc986d79c6504ff34f3
LEEBYEONGGYU <55679098+LEEBYEONGGYU@users.noreply.github.com>
2020-12-13 18:43:10 +0900

    1213
    1213

--- PHP/htdocs_게시판/page/board/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include $_SERVER['DOCUMENT_ROOT']."/db.php";
-	
-	$bno = $_GET['idx'];
-	$sql = mq("delete from board where idx='$bno';");
-
-	echo "<script>alert('삭제되었습니다.');</script>";
-?>
-<meta http-equiv="refresh" content="0 url=/" />
--- PHP/게시판/160702/db.php
+++ PHP/게시판/160702/db.php
@@ -2,7 +2,8 @@
 	session_start();
 	header('Content-Type: text/html; charset=utf-8'); // utf-8인코딩
 
-	$db = new mysqli("localhost","root","xampp","post_board");
+	// localhost = DB주소, web=DB계정아이디, 1234=DB계정비밀번호, post_board="DB이름"
+	$db = new mysqli("localhost","web","1234","post_board"); 
 	$db->set_charset("utf8");
 
 	function mq($sql)

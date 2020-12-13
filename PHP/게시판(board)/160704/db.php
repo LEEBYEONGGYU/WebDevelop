f8c2a481fedfd2da1e617e2401dbb0f6343d43df
LEEBYEONGGYU <55679098+LEEBYEONGGYU@users.noreply.github.com>
2020-12-13 20:17:52 +0900

    1213

---
+++ PHP/게시판(board)/160704/db.php
@@ -0,0 +1,14 @@
+<?php
+
+	header('Content-Type: text/html; charset=utf-8'); // utf-8인코딩
+
+	// localhost = DB주소, web=DB계정아이디, 1234=DB계정비밀번호, post_board="DB이름"
+	$db = new mysqli("localhost","web","1234","post_board"); 
+	$db->set_charset("utf8");
+
+	function mq($sql)
+	{
+		global $db;
+		return $db->query($sql);
+	}
+?>
